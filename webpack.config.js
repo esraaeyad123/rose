3eb54a5496c88152538721ab625c09f593959e7e
unknown <sosoeyad@hotmail.com>
2024-08-20 16:47:34 +0300

    create custom product card

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         app     : [asset('styles/app.scss'), asset('js/wishlist.js'), asset('js/app.js')],
         home    : asset('js/home.js'),
         'my-product-card':'/js/partials/my-custom-product-card.js',
-        'product-card' : asset('js/partials/product-card.js'),
+        'custom-salla-product-card' : asset('js/partials/custom-salla-product-card.js'),
         'main-menu' : asset('js/partials/main-menu.js'),
         checkout: [asset('js/cart.js'), asset('js/thankyou.js')],
         pages   : [asset('js/loyalty.js'), asset('js/brands.js'),],
