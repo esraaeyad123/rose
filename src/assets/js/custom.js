8ef2b44e3c32f55d0460e9e77a6eefbab4cea8bb
unknown <sosoeyad@hotmail.com>
2024-10-26 08:10:38 +0300

    update

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var my_modal = document.getElementById('my_modal');
-    var span_tag = document.getElementById('show_time');
-    var countdown_call = '';
-    var time_count = 0;
-
-    function showModal(){
-        my_modal.style.display = "block";
-        // countdown after show
-        countdown_call = setInterval(updateTime, 1000); // call for every 1s
-    }
-
-    function closeModal(){
-        my_modal.style.display = "none";
-    }
-
-    // يمكن استدعاء showModal() لفتح المودال تلقائياً بعد فترة
-    // setTimeout(showModal, 1000);
-});
-
-
+function closeModal(){
+    my_modal.style.display = "none";
+}
